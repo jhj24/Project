248ed675e185e67373cf9a1639e331e0ca95ba8d
jhj24 <1406929846@qq.com>
2019-12-04 17:56:30 +0800

    1、提高定位速度 2、更新tbs

--- lib_amap_3d/src/main/java/com/zgdj/amap3d/Amap.kt
+++ lib_amap_3d/src/main/java/com/zgdj/amap3d/Amap.kt
@@ -1,8 +1,14 @@
 package com.zgdj.amap3d
 
 import android.content.Context
+import android.support.v4.app.Fragment
 import com.zgdj.lib.extention.loadingDialog
 import com.zgdj.lib.utils.PreferenceUtil
+
+
+fun Fragment.getLocation(isCurrentTime: Boolean = false, body: (LocationBean) -> Unit) {
+    context?.getLocation(isCurrentTime, body)
+}
 
 fun Context.getLocation(isCurrentTime: Boolean = false, body: (LocationBean) -> Unit) {
     val position = PreferenceUtil.find(this, "location", LocationBean::class.java)
@@ -10,20 +16,20 @@ fun Context.getLocation(isCurrentTime: Boolean = false, body: (LocationBean) -> 
     if (position != null) {
         val locTime = position.time
         if (System.currentTimeMillis() - locTime <= 5 * 60 * 1000 && !isCurrentTime) {
-            body(position)
             dialog.dismiss()
+            body(position)
         } else {
             LocationUtils(this) {
-                body(it)
                 PreferenceUtil.save(this@getLocation, it, "location")
                 dialog.dismiss()
+                body(it)
             }.start()
         }
     } else {
         LocationUtils(this) {
-            body(it)
             PreferenceUtil.save(this@getLocation, it, "location")
             dialog.dismiss()
+            body(it)
         }.start()
     }
 }
